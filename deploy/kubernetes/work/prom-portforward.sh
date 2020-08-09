a8810f688ab7ab5f26ebaf6e145e48eb8373b264
kashinoki38 <kashinoki38@gmail.com>
2020-08-09 18:17:46 +0900

    add jaeger and kiali to port forward sh

--- deploy/kubernetes/work/prom-portforward.sh
+++ deploy/kubernetes/work/prom-portforward.sh
@@ -1,1 +1,4 @@
-kubectl get po -n monitoring -l app=prometheus |awk '$3=="Running"{system ("kubectl port-forward -n monitoring "$1" 9090:9090" )}'
+kubectl get po -n monitoring -l app=prometheus |awk '$3=="Running"{system ("kubectl port-forward -n monitoring "$1" 9090:9090" )}' &
+kubectl port-forward svc/tracing 9411:9411 -n istio-system &
+kubectl port-forward svc/kiali 20001:20001 -n istio-system &
+
